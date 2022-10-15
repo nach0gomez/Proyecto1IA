79d3c2210173147de631ac2b38c5dfbe06181bb0
nachog0mez <terminatitorxdyt@gmail.com>
2022-10-15 16:47:24 -0500

    cambios generales a laberinto

--- laberinto.py
+++ laberinto.py
@@ -1,35 +1,78 @@
-from _tkinter import *
-from tkinter import Button, Entry, Tk, messagebox
+
+# * cambio a la manera de importacion, ya que me generaba incosistencias al
+# * momento de llamar los metodos de tkinter
+import tkinter as tk
 from tkinter.ttk import Label
+
+
 class Laberinto():
     def __init__(self, master): #inicializar clase
         super().__init__(master)
         self.pack()
 
-laberinto=Tk ()
+laberinto=tk.Tk()
 #Titulo
 laberinto.title("Laberinto")
 #definir un tamaño fijo en booleano
 laberinto.resizable(True,True) #(width(ancho),height(alto))
 #cambiar icono de la interfaz
 #raiz.iconbitmap()
-#predeterminar el tamaño
-laberinto.geometry("350x150")
+
+
+appAncho = 350
+appAlto = 150
+
+# * determinamos, basados en el tamano de la pantalla,
+# * para centrar la interfaz
+
+screen_width = laberinto.winfo_screenwidth()
+screen_height = laberinto.winfo_screenheight()
+x = (screen_width / 2) - (appAncho / 2)
+y = (screen_height / 2) - (appAlto / 2)
+
+# * predeterminar el tamaño
+laberinto.geometry(f'{appAncho}x{appAlto}+{int(x)}+{int(y)}')
+
+#laberinto.geometry("350x150")
+
 #color de fondo
 laberinto.config(bg="orange")
 
+# C:/Python33/README.txt
 #def newlaberinto():
 #    messagebox.showinfo(message= "laberinto cargado", title="newlaberinto")
-lbl = Label (laberinto, text= "Ingresa el nombre del archivo que contiene el laberinto:")
-lbl.place(x=30, y= 10)
-inp1 = Entry(laberinto)
-inp1.place(x=110, y= 45)
+lbl = tk.Label (laberinto, bg='orange' ,text= "Ingresa la ruta del archivo que contiene el laberinto:")
+lbl.place(x = 30, y = 10)
+lbl1 = tk.Label(laberinto, bg='orange', text= "Ej:C:/Python33/matriz.txt")
+lbl1.place(x = 30, y = 40)
+inpRuta = tk.Entry(laberinto)
+inpRuta.place(x = 170, y = 40)
 
-btn1 = Button (laberinto, text= "Aceptar")
+
+# * metodo para leer la matriz desde un archivo de texto
+# * nombramos el objeto de tipo archivo como file_object y guardamos el contenido 
+# * del texto en la variable leer
+# * with es para que cuando no lo utilice mas (el objeto), cierre el intercambio de datos
+
+def buscarRuta():
+    ruta=inpRuta.get()
+    with open(ruta) as file_object:
+     leer = file_object.readlines()
+    print(leer)
+    
+# * boton para buscar el archivo en la ruta especificada
+btnAceptar = tk.Button (laberinto, text= "Aceptar", command=buscarRuta)
 #command=newlaberinto
-btn1.place(x=60, y= 90)
-btn2 = Button (laberinto, text = "Cancelar")
-btn2.place(x=220, y=90)
+btnAceptar.place(x = 60, y = 90)
+
+# * boton para cancelar el juego y cerrarlo
+btnCancelar = tk.Button (laberinto, text = "Cancelar", command=laberinto.destroy)
+btnCancelar.place(x = 220, y = 90)
+
+
+
+
 
 #siempre debe estar al final
 laberinto.mainloop() #ejecucion en bucle infinito de la interfaz
+
