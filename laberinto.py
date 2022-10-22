11b7496277c7ae9dcd235276b2a73e99ad277baa
camilaarcos <maria.arcos@correounivalle.edu.co>
2022-10-22 09:21:20 -0500

    interfaz principal actualizada

--- laberinto.py
+++ laberinto.py
@@ -1,13 +1,15 @@
 
 # * cambio a la manera de importacion, ya que me generaba incosistencias al
 # * momento de llamar los metodos de tkinter
+from asyncio.windows_events import NULL
 import tkinter as tk
 from tkinter.ttk import Label
+from tkinter import messagebox
 
 
 class Laberinto():
-    def __init__(self, master): #inicializar clase
-        super().__init__(master)
+    def __init__(self): #inicializar clase
+        super().__init__()
         self.pack()
 
 laberinto=tk.Tk()
@@ -17,21 +19,20 @@ laberinto.title("Laberinto")
 laberinto.resizable(True,True) #(width(ancho),height(alto))
 #cambiar icono de la interfaz
 #raiz.iconbitmap()
+laberintoAncho = 350
+laberintoAlto = 150  
 
-
-appAncho = 350
-appAlto = 150
 
 # * determinamos, basados en el tamano de la pantalla,
 # * para centrar la interfaz
 
 screen_width = laberinto.winfo_screenwidth()
 screen_height = laberinto.winfo_screenheight()
-x = (screen_width / 2) - (appAncho / 2)
-y = (screen_height / 2) - (appAlto / 2)
+x = (screen_width / 2) - (laberintoAncho / 2)
+y = (screen_height / 2) - (laberintoAlto / 2)
 
 # * predeterminar el tamaño
-laberinto.geometry(f'{appAncho}x{appAlto}+{int(x)}+{int(y)}')
+laberinto.geometry(f'{laberintoAncho}x{laberintoAlto}+{int(x)}+{int(y)}')
 
 #laberinto.geometry("350x150")
 
@@ -55,16 +56,22 @@ inpRuta.place(x = 170, y = 40)
 # * with es para que cuando no lo utilice mas (el objeto), cierre el intercambio de datos
 
 def buscarRuta():
-    ruta=inpRuta.get()
+    ruta=inpRuta.get() 
     with open(ruta) as file_object:
-     leer = file_object.readlines()
-    print(leer)
-    
+        leer = file_object.readlines()
+        print(leer)
+
+def ruta1(): 
+     if len(inpRuta.get()) == 0:
+         messagebox.showinfo('Error', 'Ingrese una ruta valida') 
+     else:
+          buscarRuta() 
+          messagebox.showinfo ('Mensaje', 'Ingresado correctamente')
+
 # * boton para buscar el archivo en la ruta especificada
-btnAceptar = tk.Button (laberinto, text= "Aceptar", command=buscarRuta)
+btnAceptar = tk.Button (laberinto, text= "Aceptar",command=ruta1)
 #command=newlaberinto
 btnAceptar.place(x = 60, y = 90)
-
 # * boton para cancelar el juego y cerrarlo
 btnCancelar = tk.Button (laberinto, text = "Cancelar", command=laberinto.destroy)
 btnCancelar.place(x = 220, y = 90)
