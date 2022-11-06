cb04dd1ab4aadbe667ecf00d5eb89cd9550d8d03
nachog0mez <terminatitorxdyt@gmail.com>
2022-11-06 18:00:20 -0500

    guardado de matriz en lista juego

--- laberinto.py
+++ laberinto.py
@@ -7,8 +7,13 @@ from busquedabidireccional import *
 
 
 class Laberinto():
-    def __init__(self): #inicializar clase
+   
+   
+    def __init__(self) -> None:
         self.mostrar()
+        
+        
+
 
     # * determinamos, basados en el tamano de la pantalla,
     # * para centrar la interfaz
@@ -35,59 +40,74 @@ class Laberinto():
         #color de fondo
         laberinto.config(bg="sky blue")
 
-        # C:/Python33/README.txt
-        #def newlaberinto():
-        #    messagebox.showinfo(message= "laberinto cargado", title="newlaberinto")
+
         lbl = tk.Label (laberinto, bg='white' ,text= "Ingresa la ruta del archivo que contiene el laberinto:")
         lbl.place(x = 30, y = 10)
-        #lbl1 = tk.Label(laberinto, bg='orange', text= "Nombre del archivo: ")
-        #lbl1.place(x = 30, y = 40)
+        
         self.inpRuta = tk.Entry(laberinto)
         self.inpRuta.place(x = 110, y = 50)
+        
 
          # * boton para buscar el archivo en la ruta especificada
-        btnAceptar = tk.Button (laberinto, text= "Aceptar",command= self.ruta1)
-        #command=newlaberinto
+        btnAceptar = tk.Button (laberinto, text= "Aceptar",command = self.ruta1)
+        
         btnAceptar.place(x = 60, y = 90)
         # * boton para cancelar el juego y cerrarlo
-        btnCancelar = tk.Button (laberinto, text = "Cancelar", command=laberinto.destroy)
+        btnCancelar = tk.Button (laberinto, text = "Cancelar", command = laberinto.destroy)
         btnCancelar.place(x = 220, y = 90)
-        #siempre debe estar al final
-        laberinto.mainloop() #ejecucion en bucle infinito de la interfaz
-
-
-    def buscarRuta(self):
-        ruta=self.inpRuta.get() 
-        archivo = open(ruta+".txt")
-        leer = archivo.read()
-        print(leer)
+        laberinto.mainloop() # * ejecucion en bucle infinito de la interfaz
         
     
+    
+    # * metodo que verifica que el archivo exista con una ruta valida, y que no se haya ingresado una ruta nula
     def ruta1(self): 
         if len(self.inpRuta.get()) == 0:
-            messagebox.showinfo('Error', 'Ingrese una ruta valida') 
+            
+            # TODO remover el self.buscarRuta
+            self.buscarRuta()
+            messagebox.showinfo('Error', 'Ingrese una ruta valida')
+            
         else:
             self.buscarRuta() 
             messagebox.showinfo ('Mensaje', 'Ingresado correctamente')
+            
 
         
-# * metodo para leer la matriz desde un archivo de texto
-# * nombramos el objeto de tipo archivo como file_object y guardamos el contenido 
-# * del texto en la variable leer
-# * with es para que cuando no lo utilice mas (el objeto), cierre el intercambio de datos
-def validarArchivo(self): 
-    ruta=self.inpRuta.get()
-    try:
-        with open(ruta) as archivo:
-            leer = archivo.read()
+        
+    # * matriz donde guardamos el juego actual
+    juego = []
+    
+    
+    # * metodo para leer la matriz desde un archivo de texto
+    # * nombramos el objeto de tipo archivo como file_object y guardamos el contenido 
+    # * del texto en la variable leer
+    # * with es para que cuando no lo utilice mas (el objeto), cierre el intercambio de datos
+    def buscarRuta(self):
+        """Metodo que nos lee el archivo y lo guarda como matriz"""
+    
+        ruta=self.inpRuta.get() 
+        
+        #TODO camiar para que nos solo busque este archivo, cambiar 'lab2' por ruta para leer el txt
+        archivo = open('lab2'+".txt")
+        leer = archivo.readline()
+        
+        
+        strAux = ""
+
+        # * aqui es donde guardamos el txt en el arreglo juego[] 2D list
+        while (leer != ""):
             print(leer)
-        messagebox.showinfo('Información', 'Archivo encontrado')
+            strAux = str(leer)
+            leer = archivo.readline()
+            self.juego.append(list(strAux))
         
-    except:
-        messagebox.showerror('Error', 'No se encontró un archivo valido')
-    finally:
-        self.inpRuta.delete(0,'end')
+        for i in range(len(self.juego)-1):
+            self.juego[i].remove('\n')
+            
+        
+        
 
-
+    
+        
 if __name__ == '__main__':
     lab = Laberinto()
